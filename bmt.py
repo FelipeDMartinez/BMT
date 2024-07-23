355a40ff152fe864b3909a4a6ab5a0b540c4a437
Luiz Felipe Domingos Martinez <168031294+FelipeDMartinez@users.noreply.github.com>
2024-07-23 01:28:51 -0300

    Update bmt.py

--- bmt.py
+++ bmt.py
@@ -1,8 +1,8 @@
 from PC import processador_de_consultas
 from GLI import gerador_de_lista_invertida
 from INDEX import indexador
-# from BUSCA import buscador
-# from AVALIA import avaliacao
+from BUSCA import buscador
+from AVALIA import avaliacao
 import time
 
 if __name__ == "__main__":
@@ -23,15 +23,15 @@ if __name__ == "__main__":
     fim_index = time.time()
     print(f'Tempo para indexador: {fim_index-ini_index}s')
 
-    # ini_busca = time.time()
-    # buscador()
-    # fim_busca = time.time()
-    # print(f'Tempo para buscador: {fim_busca-ini_busca}s')
+    ini_busca = time.time()
+    buscador()
+    fim_busca = time.time()
+    print(f'Tempo para buscador: {fim_busca-ini_busca}s')
 
-    # ini_avalia = time.time()
-    # avaliacao()
-    # fim_avalia = time.time()
-    # print(f'Tempo para avaliação: {fim_avalia-ini_avalia}s')
+    ini_avalia = time.time()
+    avaliacao()
+    fim_avalia = time.time()
+    print(f'Tempo para avaliação: {fim_avalia-ini_avalia}s')
 
     fim = time.time()
     print(f'tempo total para rodar todo o sistema {fim-ini} s')
